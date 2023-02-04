10c39ff167fb64590969a701e0bb3f3566fad7a5
Vladimir Borisov <1mt-2@mail.ru>
2023-02-04 15:38:19 +0300

    refactor5

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3010
 
-app.use(cors({origin: (origin, callback)=>{
-    callback(null, true)
-    }}))
+app.use(cors({
+        origin: (origin, callback) => {
+            callback(null, true)
+        },
+        credentials: true
+    })
+)
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
